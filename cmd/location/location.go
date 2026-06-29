d304c32173cb222d98a666e3180345e3e560b1d8
ckoegel <ckoegel1006@gmail.com>
2026-06-29 14:33:17 -0400

    location docs

--- cmd/location/location.go
+++ cmd/location/location.go
@@ -6,4 +6,5 @@ import "github.com/spf13/cobra"
 var Cmd = &cobra.Command{
 	Use:   "location",
 	Short: "Manage locations (SIP peers) under sub-accounts",
+	Long:  "Create and list locations (SIP peers) within a sub-account. Locations define the SIP endpoints that route voice traffic for the numbers assigned to them.",
 }
